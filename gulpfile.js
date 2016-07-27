ad345b43d36f669bb13152809e9ceb318f6c44ba
Quni <quniwork@gmail.com>
2016-07-27 17:24:45 +0800

    upload build to css/image

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,16 @@ var gulp = require('gulp'),
 
 //壓縮圖片
 gulp.task('gulp-imagemin', () =>
-    gulp.src('./image/*')   //來源資料夾
+    gulp.src('./buil/image/*')   //來源資料夾
         .pipe(imagemin())
-        .pipe(gulp.dest('./imagemin')) //目標資料夾, 如需覆蓋則指定跟來源相同
+        .pipe(gulp.dest('./image')) //目標資料夾, 如需覆蓋則指定跟來源相同
 );
 
 //壓縮css
 gulp.task('minify-css', function() {
-  return gulp.src('./css/*.css')
+  return gulp.src('./build/css/*.css')
     .pipe(minifyCSS({keepBreaks:true}))
-    .pipe(gulp.dest('./minify-css'))
+    .pipe(gulp.dest('./css'))
 });
 
 
