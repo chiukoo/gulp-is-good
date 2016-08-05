e80bec56da7ec8f8254161f673eda2ae3e934b6b
Quni <quniwork@gmail.com>
2016-08-05 15:25:48 +0800

    update compass-sass & js & webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,75 @@
 'use strict';
 
-var gulp = require('gulp'),
-    imagemin = require('gulp-imagemin'),
-    sass = require('gulp-sass'),
-    sassGlob = require('gulp-sass-glob'),
-    minifyCSS = require('gulp-minify-css');
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin'); //- 壓縮圖片
+var compass = require('gulp-compass');
+var concat = require('gulp-concat'); //- 合併檔案
+var minifyCSS = require('gulp-minify-css'); //- 壓縮css
+var uglify = require('gulp-uglify'); //- 混淆並壓縮
+var rename = require("gulp-rename"); //- 重新命名檔案
+var jade = require('gulp-jade');
+var webserver = require('gulp-webserver');
 
-
-//編譯sass
-gulp.task('gulp-scss', function () {
-    return gulp
-        .src('./src/*/*.scss')
-        .pipe(sassGlob())
-        .pipe(sass())
-        .pipe(gulp.dest('./dist/css'));
-});
-
-//壓縮css
-gulp.task('minify-css', function() {
-  return gulp.src('./src/css/*.css')
-    .pipe(minifyCSS({keepBreaks:true}))
-    .pipe(gulp.dest('./dist/css'))
-});
-
-//壓縮圖片
-gulp.task('gulp-imagemin', () =>
-    gulp.src('./src/image/*')   //來源資料夾
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/image')) //目標資料夾, 如需覆蓋則指定跟來源相同
+//- 壓縮圖片
+gulp.task('imagemin', () =>
+    gulp.src('assets/image/*') //來源資料夾
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/image')) //目標資料夾, 如需覆蓋則指定跟來源相同
 );
 
+//- comass-sass
+gulp.task('compass', function() {
+    return gulp.src('./assets/sass/*.scss')
+        .pipe(compass({
+            sourcemap: true,
+            time: true,
+            css: 'assets/css/',
+            sass: 'assets/sass/',
+            image: 'assets/image',
+            style: 'compressed' //- 壓縮格式nested, expanded, compact, compressed
+        }))
+        .pipe(gulp.dest('dist/css/'));
+});
 
-//要執行的事件
-gulp.task('default',['gulp-scss', 'minify-css', 'gulp-imagemin']);
+//- 合併js
+gulp.task('uglify', function() {
+    return gulp.src('assets/js/*.js')
+        .pipe(uglify())
+        .pipe(rename(function(path) {
+            // path.basename += ".min";  //- 改變檔名
+            path.extname = ".js";
+        }))
+        .pipe(gulp.dest('dist/js/'));
+});
+
+//- jade
+gulp.task('templates', function() {
+  var YOUR_LOCALS = {};
+
+  gulp.src('./assets/*.jade')
+    .pipe(jade({
+      locals: YOUR_LOCALS
+    }))
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('webserver', function() {
+  gulp.src('dist/')
+    .pipe(webserver({
+      port: 8080,
+      livereload: true,
+      directoryListing: false,
+      open: true,
+      fallback: '*'
+    }));
+});
+
+//- 監聽的事件
+gulp.task('watch', function() {
+    gulp.watch('assets/sass/*.scss', ['compass']);
+    gulp.watch('assets/js/*.js', ['uglify']);
+    gulp.watch('assets/*.jade', ['templates']);
+});
+
+//- 執行的事件
+gulp.task('default', ['webserver', 'templates', 'imagemin', 'compass', 'uglify', 'watch']);
